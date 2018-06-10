ece7b90c7bff49428969396ebcff2e30ce67ec40
Erik <erik.illaste@gmail.com>
2018-06-10 16:13:19 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+// in ES6 - no node support
+// import express from 'express';
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ hi: 'there' });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
